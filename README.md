c37a91725659bf8b330cf8589c4f72ebd051bb9c
Commi You <commiyou@gmail.com>
2023-05-25 00:13:48 +0800

    add disableJavaScript

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Webpage to PDF action with javascript disabled
+# Webpage to PDF action with Chinese font
+
+This action is based on the [misaelnieto/web_to_pdf_action](https://github.com/misaelnieto/web_to_pdf_action) repository. Modifications have been made to support Chinese fonts.
 
 A [GitHub Action](https://github.com/features/actions) to convert any webpage to a PDF document.
 
@@ -9,6 +11,7 @@ Internally uses Puppeteer's mojo to get the page and render it to print it to PD
 * `webPageURL`: This is the URL of the page. **Required**.
 * `outputFile`: Path to the generated PDF file. **Required**.
 * `usePuppeteer`: If you need to use Puppeteer or your chrome.
+* `disableJavaScript`: If you need to disable js, default false
 * `useScreen`: If you want to use the option emulateMediaType to generate with screen  media and not print media
 * `pdfOptions`: PDF options as described on [Puppeteer's documentation](https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions). This is optional.
 
@@ -16,12 +19,13 @@ Internally uses Puppeteer's mojo to get the page and render it to print it to PD
 
 ```yml
 - name: html to pdf
-  uses: misaelnieto/web_to_pdf_action@master
+  uses: commiyou/web_to_pdf_action@master
   with:
     webPageURL: https://www.noenieto.com/resume/
     outputFile: ./path/to/my/resume.pdf
     usePuppeteer: true
     useScreen: true
+    disableJavaScript: true
     pdfOptions: '{"format": "Letter", "margin": {"top": "10mm", "left": "10mm", "right": "10mm", "bottom": "10mm"}}'
 ```
 
--- action.js
+++ action.js
@@ -3,53 +3,54 @@ const core = require('@actions/core');
 const puppeteer = require('puppeteer');
 
 (async () => {
-    try {
-        const googlePath = '/opt/google/chrome/chrome';
-        const webPageURL = core.getInput('webPageURL');
-        const outputFile = core.getInput('outputFile');
-        const usePuppeteer = core.getInput('usePuppeteer');
-        const useScreen = core.getInput('useScreen');
-        console.log(`Starting PDF generation for ${webPageURL}`);
-        if (usePuppeteer) {
-            const pdfDefaults = {
-                'displayHeaderFooter': false,
-                'path': outputFile
-            };
-            const pdfOpts = Object.assign({}, JSON.parse(core.getInput('pdfOptions')), pdfDefaults);
-            const pptrOpts = {
-                executablePath: googlePath,
-                args: ['--no-sandbox', '--headless', '--disable-gpu']
-            };
+  try {
+    const googlePath = '/opt/google/chrome/chrome';
+    const webPageURL = core.getInput('webPageURL');
+    const outputFile = core.getInput('outputFile');
+    const usePuppeteer = core.getInput('usePuppeteer');
+    const disableJS = core.getInput('disableJavaScript');
+    const useScreen = core.getInput('useScreen');
+    console.log(`Starting PDF generation for ${webPageURL}`);
+    if (usePuppeteer) {
+      const pdfDefaults = {
+        'displayHeaderFooter': false,
+        'path': outputFile
+      };
+      const pdfOpts = Object.assign({}, JSON.parse(core.getInput('pdfOptions')), pdfDefaults);
+      const pptrOpts = {
+        executablePath: googlePath,
+        args: ['--no-sandbox', '--headless', '--disable-gpu']
+      };
 
-            const browser = await puppeteer.launch(pptrOpts);
-            const daPage = await browser.newPage();
-            daPage.setJavaScriptEnabled(false);
-            await daPage.goto(webPageURL, {
-                waitUntil: "networkidle0",
-            })
-            if (useScreen) {
-                await daPage.emulateMediaType('screen');
-            }
-            await daPage.pdf(pdfOpts)
-            await browser.close()
+      const browser = await puppeteer.launch(pptrOpts);
+      const daPage = await browser.newPage();
+      if (disableJS) daPage.setJavaScriptEnabled(false);
+      await daPage.goto(webPageURL, {
+        waitUntil: "networkidle0",
+      })
+      if (useScreen) {
+        await daPage.emulateMediaType('screen');
+      }
+      await daPage.pdf(pdfOpts)
+      await browser.close()
 
-        } else {
-            const { exec } = require("child_process");
-            exec(`${googlePath} --no-sandbox --headless --disable-gpu --print-to-pdf="${outputFile}" ${webPageURL}`, (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-            });
+    } else {
+      const { exec } = require("child_process");
+      exec(`${googlePath} --no-sandbox --headless --disable-gpu --print-to-pdf="${outputFile}" ${webPageURL}`, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
         }
-        console.log(`DONE. Generated PDf file is at ${outputFile}`)
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
     }
-    catch (error) {
-        core.setFailed(error.message);
-    }
+    console.log(`DONE. Generated PDf file is at ${outputFile}`)
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
 })();
